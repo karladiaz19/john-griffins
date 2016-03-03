2972ec4df2b77ad1e691bace7d62d981da721117
Ashante <ashantejr@gmail.com>
2016-03-03 22:20:57 +0000

    ashante is bae

--- app.rb
+++ app.rb
@@ -1,18 +1,17 @@
 require_relative 'config/environment'
 
-class app < Sinatra::Base 
+class App < Sinatra::Base 
 
   get '/' do
    erb :dashboard
   end 
-  
-end
 
 post '/' do
   @user = {
    name: "#{params["fname"]} #{params["lname"]}" 
     }
- erb:dashboard
- end
+  
+ erb :dashboard
+  end
 
 end
